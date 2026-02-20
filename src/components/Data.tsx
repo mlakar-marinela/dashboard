2b33e90455886d522103b5fc609c060b072c60a4
mlakar-marinela <marinelamlakar03@gmail.com>
2026-02-20 20:11:58 +0200

    Changes

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -51,7 +51,7 @@ function Data() {
       <input type="text" id="data2"  placeholder="Write the turnover"/>
       <label>Profit</label>
       <input type="text" id="data3" placeholder="Write the profit"/>
-      <input type="submit" value="Submit" onClick={handleSubmit} />
+      <input type="submit" value="Submit" data-testid ='submit_button' onClick={handleSubmit} />
     </form>
   );
 }
--- src/tests/Data.test.tsx
+++ src/tests/Data.test.tsx
@@ -2,6 +2,7 @@ import Data from '../components/Data'
 import {test, expect} from 'vitest'
 import {page} from 'vitest/browser'
 import { renderWithProvider } from '../utils/test_utils'
+import reducer, {changing_data} from '../state_management/dataSlice'
 
 test("Form labels should be visible", async() =>{
     await renderWithProvider(<Data/>);
@@ -21,8 +22,34 @@ test("All types of inputs should be visible", async() => {
       const selectYear = page.getByPlaceholder('Write the turnover');
       const turnoverInput = page.getByPlaceholder('Write the turnover');
       const profitInput = page.getByPlaceholder('Write the profit');
+      const submitButton = page.getByTestId("submit_button");
 
       expect(selectYear).toBeVisible();
       expect(turnoverInput).toBeVisible();
       expect(profitInput).toBeVisible();
-})
+      expect(submitButton).toBeVisible();
+});
+
+test("Reducer should update turnover and profit correctly", () => {
+
+  const initialState = {
+    financialDatas: [
+      { year: 2022, turnover: 0, profit: 0 },
+      { year: 2023, turnover: 0, profit: 0 },
+      { year: 2024, turnover: 0, profit: 0 }
+    ]
+  };
+
+  const action = changing_data({
+    year: 2022,
+    turnover: 500,
+    profit: 100
+  });
+
+  const newState = reducer(initialState, action);
+
+  const updatedFinancialData = newState.financialDatas.find(data => data.year === 2022);
+
+  expect(updatedFinancialData?.turnover).toBe(500);
+  expect(updatedFinancialData?.profit).toBe(100);
+});
